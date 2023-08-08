74d568a568259db940de681811a0bf99e26b5395
Marcus Isaksson <me@naknut.email>
2023-08-08 13:54:14 +0200

    Now reads analog value in app task

--- src/app_task.cpp
+++ src/app_task.cpp
@@ -34,6 +34,13 @@
 
 #include <app-common/zap-generated/attributes/Accessors.h>
 
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/adc.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+
 LOG_MODULE_DECLARE(app, CONFIG_CHIP_APP_LOG_LEVEL);
 
 using namespace ::chip;
@@ -88,6 +95,20 @@ app::Clusters::NetworkCommissioning::Instance
 constexpr size_t kMeasurementsIntervalMs = 3000;
 
 k_timer sSensorTimer;
+
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "No suitable devicetree overlay specified"
+#endif
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+/* Data of ADC io-channels specified in devicetree. */
+static const struct adc_dt_spec adc_channels[] = {
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+			     DT_SPEC_AND_COMMA)
+};
 
 void SensorTimerHandler(k_timer *timer)
 {
@@ -189,9 +210,25 @@ CHIP_ERROR AppTask::Init()
 		return err;
 	}
 
+	/* Initialize sensor timers*/
 	k_timer_init(&sSensorTimer, &SensorTimerHandler, nullptr);
 	k_timer_user_data_set(&sSensorTimer, this);
 	k_timer_start(&sSensorTimer, K_MSEC(kMeasurementsIntervalMs), K_MSEC(kMeasurementsIntervalMs));
+
+	/* Initialize ADC */
+	/* Configure channels individually prior to sampling. */
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+			if (!device_is_ready(adc_channels[i].dev)) {
+				printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+		 		return chip::System::MapErrorZephyr(-ENODEV);
+		 	}
+
+ 		int err = adc_channel_setup_dt(&adc_channels[i]);
+ 		if (err < 0) {
+ 			printk("Could not setup channel #%d (%d)\n", i, err);
+ 			return chip::System::MapErrorZephyr(-ENODEV);
+ 		}
+	}
 
 	return CHIP_NO_ERROR;
 }
@@ -367,6 +404,49 @@ void AppTask::DispatchEvent(const AppEvent &event)
 
 void AppTask::SensorMeasureHandler(const AppEvent &)
 {
+	uint16_t buf;
+	struct adc_sequence sequence = {
+		.buffer = &buf,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(buf),
+	};
+
+	printk("ADC reading\n");
+ 	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+ 		int32_t val_mv;
+
+ 		printk("- %s, channel %d: ",
+ 		       adc_channels[i].dev->name,
+		       adc_channels[i].channel_id);
+
+ 		(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+
+ 		int err = adc_read(adc_channels[i].dev, &sequence);
+ 		if (err < 0) {
+ 			printk("Could not read (%d)\n", err);
+ 			continue;
+ 		}
+
+		/*
+		 * If using differential mode, the 16 bit value
+		 * in the ADC sample buffer should be a signed 2's
+		 * complement value.
+		 */
+		if (adc_channels[i].channel_cfg.differential) {
+			val_mv = (int32_t)((int16_t)buf);
+		} else {
+			val_mv = (int32_t)buf;
+		}
+		printk("%" PRId32, val_mv);
+		err = adc_raw_to_millivolts_dt(&adc_channels[i], &val_mv);
+		/* conversion to mV may not be supported, skip if not */
+		if (err < 0) {
+			printk(" (value in mV not available)\n");
+		} else {
+			printk(" = %" PRId32 " mV\n", val_mv);
+		}
+	}
+
 	EmberAfStatus status;
 	status = chip::app::Clusters::RelativeHumidityMeasurement::Attributes::MeasuredValue::Set(
                 /* endpoint ID */ 1, /* temperature in 0.01*C */ int16_t(rand() % 100000));
--- src/main.cpp
+++ src/main.cpp
@@ -21,20 +21,6 @@
 
 LOG_MODULE_REGISTER(app, CONFIG_CHIP_APP_LOG_LEVEL);
 
-#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
-	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
-#error "No suitable devicetree overlay specified"
-#endif
-
-#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
-	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
-
-/* Data of ADC io-channels specified in devicetree. */
-static const struct adc_dt_spec adc_channels[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
-			     DT_SPEC_AND_COMMA)
-};
-
 int main()
 {
 	// int err2;
